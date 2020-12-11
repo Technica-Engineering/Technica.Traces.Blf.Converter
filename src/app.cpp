932d344a4094d7b6a6ed63f6a459b29cb265a50b
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2020-12-11 10:31:47 +0100

    Add new line in usage

--- src/app.cpp
+++ src/app.cpp
@@ -315,7 +315,7 @@ void write(light_pcapng outfile, EthernetFrameForwarded* obj) {
 
 int main(int argc, char* argv[]) {
 	if (argc != 3) {
-		fprintf(stderr, "Usage %s [infile] [outfile]", argv[0]);
+		fprintf(stderr, "Usage %s [infile] [outfile]\n", argv[0]);
 		return 1;
 	}
 
