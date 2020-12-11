e93ad96fd2356a28809743b7c568fe335d2ca5de
Dr. Lars Völker <28217814+LarsVoelker@users.noreply.github.com>
2020-12-11 18:01:26 +0100

    Bugfix: Timestamping in BLF (#4)
    * Bugfix: Timestamping in BLF
    * Use vector_blf fork
    
    Co-authored-by: Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>

--- cmake/pcapng.cmake
+++ cmake/pcapng.cmake
@@ -5,6 +5,6 @@ include(FetchContent)
 FetchContent_Declare(
   pcapng
   GIT_REPOSITORY    https://github.com/Technica-Engineering/LightPcapNg.git
-  GIT_TAG           1debafc
+  GIT_TAG           a35bf6e
 )
 FetchContent_MakeAvailable(pcapng)
