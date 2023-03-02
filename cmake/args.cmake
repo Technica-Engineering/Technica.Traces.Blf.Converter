3ce822adf565adab2af02c2447947d9be21df5f5
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2023-03-02 17:44:08 +0100

    Upgrade args to 6.4.6 (#13)

--- cmake/args.cmake
+++ cmake/args.cmake
@@ -5,7 +5,7 @@ include(FetchContent)
 FetchContent_Declare(
   args
   GIT_REPOSITORY    https://github.com/Taywee/args.git
-  GIT_TAG           6.2.6
+  GIT_TAG           6.4.6
   CMAKE_CACHE_ARGS  "-DARGS_MAIN_PROJECT:BOOL=OFF"
 )
 
