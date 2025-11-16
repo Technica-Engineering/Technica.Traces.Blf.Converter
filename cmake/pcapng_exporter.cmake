388a4ce70089fc03cc15add3dea9f3b1c56fe4b7
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2025-11-16 14:17:34 +0100

    Fix compatiblity with camke v4.x (#29)

--- cmake/pcapng_exporter.cmake
+++ cmake/pcapng_exporter.cmake
@@ -5,6 +5,6 @@ include(FetchContent)
 FetchContent_Declare(
   pcapng_exporter
   GIT_REPOSITORY    https://github.com/Technica-Engineering/Technica.Traces.Pcapng.Exporter.git
-  GIT_TAG           8ca0443
+  GIT_TAG           b52e844
 )
 FetchContent_MakeAvailable(pcapng_exporter)
---
+++ conanfile.py
@@ -0,0 +1,47 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeDeps, CMakeToolchain, cmake_layout
+
+
+class PkgRecipe(ConanFile):
+    name = "blf_converter"
+    version = "0.1"
+    package_type = "application"
+
+    # Binary configuration
+    settings = "os", "compiler", "build_type", "arch"
+
+    # Sources are located in the same place as this recipe, copy them to the recipe
+    exports_sources = "CMakeLists.txt", "src/*"
+
+    def layout(self):
+        cmake_layout(self, "Ninja")
+
+    def requirements(self):
+        self.requires("libpcap/1.10.5")
+        self.requires("zstd/1.5.7")
+        self.requires("taywee-args/6.4.6")
+        self.requires("tinyxml2/11.0.0")
+        self.requires("zlib/1.3.1")
+
+
+    def configure(self):
+        self.options["libpcap"].shared = False
+        self.options["zstd"].shared = False
+        self.options["taywee-args"].shared = False
+        self.options["tinyxml2"].shared = False
+        self.options["zlib"].shared = False
+
+    def generate(self):
+        deps = CMakeDeps(self)
+        deps.generate()
+        tc = CMakeToolchain(self)
+        tc.generate()
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def package(self):
+        cmake = CMake(self)
+        cmake.install()
